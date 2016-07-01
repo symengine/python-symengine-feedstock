7bfa7cc70b03c671190e7f7192344db60d062d43
Isuru Fernando <isuruf@gmail.com>
2016-07-01 08:55:10 +0530

    Update symengine-token

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -14,7 +14,7 @@ config=$(cat <<CONDARC
 
 channels:
  - conda-forge
-
+ - symengine
  - defaults # As we need conda-build
 
 conda-build:
@@ -48,17 +48,17 @@ conda info
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root symengine --channel=main || exit 1
 
     set -x
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root symengine --channel=main || exit 1
 
     set -x
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root symengine --channel=main || exit 1
 EOF
