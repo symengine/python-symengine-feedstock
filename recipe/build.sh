dacb4d5b19ded2cd017dd3217aa2e494d7094252
Isuru Fernando <isuruf@gmail.com>
2016-09-27 09:21:02 +0530

    Set LD_LIBRARY_PATH
    otherwise the cmake executable cannot find libarchive.so.13

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source activate "${CONDA_DEFAULT_ENV}"
-
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 python setup.py install --symengine-dir=$PREFIX --single-version-externally-managed --record record.txt
